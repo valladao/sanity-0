f04274590f3584275d990e6603f53f6e93064866
Manoel Valladao <valladao@gmail.com>
2022-01-11 06:17:15 -0300

    Adding categories

--- web/pages/post/[slug].js
+++ web/pages/post/[slug].js
@@ -1,20 +1,33 @@
 // [slug].js
 
+import groq from 'groq'
 import client from '../../client'
 
-const Post = (props) => {
-  const { title = 'Missing title', name = 'Missing name' } = props.post
+const Post = ({post}) => {
+  const { title = 'Missing title', name = 'Missing name', categories } = post
   return (
     <article>
       <h1>{title}</h1>
       <span>By {name}</span>
+      {categories && (
+        <ul>
+          Posted in
+          {categories.map(category => <li key={category}>{category}</li>)}
+        </ul>
+      )}
     </article>
   )
 }
 
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title
+}`
+
 export async function getStaticPaths() {
   const paths = await client.fetch(
-    `*[_type == "post" && defined(slug.current)][].slug.current`
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
   )
 
   return {
@@ -26,14 +39,11 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   // It's important to default the slug so that it doesn't return "undefined"
   const { slug = "" } = context.params
-  const post = await client.fetch(`
-    *[_type == "post" && slug.current == $slug][0]{title, "name": author->name}
-  `, { slug })
+  const post = await client.fetch(query, { slug })
   return {
     props: {
       post
     }
   }
 }
-
 export default Post
