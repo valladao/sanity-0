d4614c535124f165e4143ffa14ac472644d4b446
Manoel Valladao <valladao@gmail.com>
2022-01-11 06:09:13 -0300

    Add a byline with author and categories

--- web/pages/post/[slug].js
+++ web/pages/post/[slug].js
@@ -1,10 +1,13 @@
+// [slug].js
+
 import client from '../../client'
 
-const Post = ({post}) => {
-
+const Post = (props) => {
+  const { title = 'Missing title', name = 'Missing name' } = props.post
   return (
     <article>
-      <h1>{post?.slug?.current}</h1>
+      <h1>{title}</h1>
+      <span>By {name}</span>
     </article>
   )
 }
@@ -24,7 +27,7 @@ export async function getStaticProps(context) {
   // It's important to default the slug so that it doesn't return "undefined"
   const { slug = "" } = context.params
   const post = await client.fetch(`
-    *[_type == "post" && slug.current == $slug][0]
+    *[_type == "post" && slug.current == $slug][0]{title, "name": author->name}
   `, { slug })
   return {
     props: {
