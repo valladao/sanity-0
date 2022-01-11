e25bc138a3af80532f746640b68abd9aafdf8349
Manoel Valladao <valladao@gmail.com>
2022-01-11 05:47:49 -0300

    Get some content from Sanity

--- web/client.js
+++ web/client.js
@@ -3,6 +3,7 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
   projectId: 'b9dcewew', // you can find this in sanity.json
-  dataset: 'production', // or the name you chose in step 1
+  dataset: 'production',
+  apiVersion: '2021-08-31', // or the name you chose in step 1
   useCdn: true // `false` if you want to ensure fresh data
 })
--- web/pages/post/[slug].js
+++ web/pages/post/[slug].js
@@ -1,14 +1,36 @@
-// post.js
-import { useRouter } from 'next/router'
+import client from '../../client'
 
-const Post = () => {
-  const router = useRouter()
+const Post = ({post}) => {
 
   return (
     <article>
-      <h1>{router.query.slug}</h1>
+      <h1>{post?.slug?.current}</h1>
     </article>
   )
+}
+
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(`
+    *[_type == "post" && slug.current == $slug][0]
+  `, { slug })
+  return {
+    props: {
+      post
+    }
+  }
 }
 
 export default Post
